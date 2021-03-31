7060c722cdbe884aeaa12ce775015e6927e95332
Yaroslav <beyaroslav@gmail.com>
2021-03-31 22:52:06 +0700

    hw5 task 1 is done

--- algoritm-hw5/src/Main.java
+++ algoritm-hw5/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(recExponentiation(2,8));
+    }
 
+    public static long recExponentiation(int base, int exp) {
+        if (exp == 1) return base;
+        return base * recExponentiation(base, exp - 1);
     }
 }
