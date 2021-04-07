bcdf571a34adb71e2ea901875b6a9f9d921a6e99
Yaroslav <beyaroslav@gmail.com>
2021-04-07 23:24:27 +0700

    Homework 7 is done

--- algoritm-hw7/src/DepthFirstPath.java
+++ algoritm-hw7/src/DepthFirstPath.java
@@ -1,14 +1,7 @@
-import java.util.LinkedList;
-
-public class DepthFirstPath {
-    private boolean[] marked;
-    private int[] edgeTo;
-    private int source;
+public class DepthFirstPath extends GraphFirstPath {
 
     public DepthFirstPath(Graph g, int source) {
-        this.source = source;
-        marked = new boolean[g.getVertexCount()];
-        edgeTo = new int[g.getVertexCount()];
+        super(g, source);
         dfs(g, source);
     }
 
@@ -20,22 +13,5 @@ public class DepthFirstPath {
                 dfs(g, w);
             }
         }
-    }
-
-    public boolean hasPathTo(int w) {
-        return marked[w];
-    }
-
-    public LinkedList<Integer> pathTo(int w) {
-        if (!hasPathTo(w)) {
-            return null;
-        }
-        LinkedList<Integer> stack = new LinkedList<>();
-        int vertex = w;
-        while (vertex != source) {
-            stack.push(vertex);
-            vertex = edgeTo[vertex];
-        }
-        return stack;
     }
 }
